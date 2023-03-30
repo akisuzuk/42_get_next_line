5b73f7eba0f13189889b32b34fca65e3806565e9
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-03-30 23:37:12 +0900

    20230330

--- test_operation.c
+++ test_operation.c
@@ -6,24 +6,43 @@
 /*   By: akisuzuk <akisuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 20:46:03 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/03/29 20:47:12 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/03/30 23:31:47 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	main(int argc, char **argv)
-{
-	int		fd;
+//int	main(int argc, char **argv)
+//{
+//	int		fd;
 
-	if (argc != 2)
-		return (0);
-	else
-	{
-		// 読み込み専用
-		fd = open(argv[1], O_RDONLY);
-		get_next_line(fd);
-		close(fd);
-		return (0);
-	}
+//	if (argc != 2)
+//		return (0);
+//	else
+//	{
+//		// 読み込み専用
+//		fd = open(argv[1], O_RDONLY);
+//		get_next_line(fd);
+//		close(fd);
+//		return (0);
+//	}
+//}
+
+int main()
+{
+    int        fd;
+    char    *line;
+
+    fd = open("./test/first_file", O_RDONLY);
+    while (1)
+    {
+        line = get_next_line(fd);
+        close (fd);
+        if (line == NULL)
+            break;
+        printf("%s", line);
+        free(line);
+    }
+    system("leaks a.out");
+    return (0);
 }
