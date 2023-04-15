ed558f4abf2847e6e51401c368d39645a0a53347
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-04-15 20:24:37 +0900

    TIMEOUT clear(changed n from size_t -> ssize_t)

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,37 @@
 /*   By: akisuzuk <akisuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 20:46:20 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/04/15 15:37:57 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/04/15 20:22:44 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	search_kaigyo(char **line, char **head, char *buf)
+static	void	finish(char **buf, char **line, char **head, int search_flag)
+{
+	free(*buf);
+	if (search_flag == -1)
+	{
+		free(*line);
+		free(*head);
+		*line = NULL;
+	}
+}
+
+static	char	*makebuf(char **line, int *search_flag)
+{
+	char	*buf;
+
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (buf == NULL)
+	{
+		free(*line);
+		*search_flag = -1;
+	}
+	return (buf);
+}
+
+static	int	search_kaigyo(char **line, char **head, char *buf)
 {
 	int		index;
 	int		search_flag;
@@ -28,13 +52,10 @@ int	search_kaigyo(char **line, char **head, char *buf)
 	search_flag = 1;
 	if (buf[index] == '\n')
 	{
+		free(temp);
 		temp = ft_strdup(buf + index + 1);
 		if (temp == NULL)
-		{
-			free(*line);
-			*line = NULL;
 			return (-1);
-		}
 		search_flag = 0;
 	}
 	free(*head);
@@ -42,13 +63,29 @@ int	search_kaigyo(char **line, char **head, char *buf)
 	return (search_flag);
 }
 
+//static	int	n_judge(ssize_t *n, char **line, char **buf, char **head)
+//{
+//	if (*n <= 0)
+//	{
+//		if (**line != '\0')
+//			return (0);
+//		else
+//			return (-1);
+//	}
+//	else
+//	{
+//		*(buf[*n]) = '\0';
+//		return (search_kaigyo(line, head, *buf));
+//	}
+//}
+
 char	*get_next_line(int fd)
 {
 	char		*line;
 	static char	*head;
 	char		*buf;
 	int			search_flag;
-	size_t		n;
+	ssize_t		n;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -58,37 +95,25 @@ char	*get_next_line(int fd)
 	*line = '\0';
 	search_flag = 1;
 	if (head)
-	{
 		search_flag = search_kaigyo(&line, &head, head);
-	}
-	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (buf == NULL)
-	{
-		free(line);
-		search_flag = -1;
-	}
+	buf = makebuf(&line, &search_flag);
 	while (search_flag == 1)
 	{
 		n = read(fd, buf, BUFFER_SIZE);
+		//search_flag = n_judge(&n, &line, &buf, &head);
 		if (n <= 0)
 		{
 			if (*line != '\0')
 				search_flag = 0;
 			else
 				search_flag = -1;
-			break ;
 		}
-		buf[n] = '\0';
-		search_flag = search_kaigyo(&line, &head, buf);
+		else
+		{
+			buf[n] = '\0';
+			search_flag = search_kaigyo(&line, &head, buf);
+		}
 	}
-	free(buf);
-	if (search_flag == -1)
-	{
-		free(line);
-		free(head);
-		line = NULL;
-		head = NULL;
-	}
-	//printf("line mojicode = %X\n", *line);
+	finish(&buf, &line, &head, search_flag);
 	return (line);
 }
